c50ca3e6ca7333da2254ea54ba3a304de854f54b
ahmed sokar <ahmed.sokar@fawry.com>
2022-07-04 08:15:21 +0200

    Organizing some files before doing the magic.

---
+++ ShoppingCart/Main/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  ShoppingCart
+//
+//  Created by admin on 03/07/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: - Properties
+    
+    var window: UIWindow?
+    
+    // MARK: - Methods
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
